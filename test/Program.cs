78b708fdfb9d49c7f0cabc3f4fbcdb84aca3c6bc
hugjil <yusang_hugjil@163.com>
2018-02-05 10:41:37 +0800

    test
    test1

---
+++ test/Program.cs
@@ -0,0 +1,26 @@
+﻿using DFrame.DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<int> list = new List<int>();
+            //list.Select(x=>x>3);
+            Assembly assembly = Assembly.Load("Models");
+            SQLFactory.Create(SQLFactory.DatabaseType.MySQL, "test1", assembly);
+            //Action call = delegate { SQLFactory.Create(DatabaseType.MySQL, "test1", assembly); };
+            ////call += delegate { SQLFactory.Create(DatabaseType.MSSQLServer, "test1", assembly); };
+            ////call.Invoke();
+            //Task.Run(call);
+            //ActionList model = new ActionList();
+            Console.ReadKey();
+        }
+    }
+}
